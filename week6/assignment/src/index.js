10638d7596d46214b36a55b82bfc7e15f3eb0f41
rbgksqkr <rbgks1937@naver.com>
2023-04-30 19:22:58 +0900

    Feat: input 입력안했을 때 오류처리

--- week6/assignment/src/index.js
+++ week6/assignment/src/index.js
@@ -1,22 +1,47 @@
 import Car from "./DOM/Car.js";
 
-const input = document.getElementById("car-names-input");
-console.log("input: ", input);
-input.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    if (input.value.includes(",")) {
-      const cars = input.value.split(",");
-      cars.forEach((car) => {
-        car && new Car(car);
-      });
-    } else {
-      new Car(input.value);
-    }
-    input.value = "";
-  }
-});
+const inputName = document.getElementById("car-names-input");
+const inputCount = document.getElementById("racing-count-input");
 
-const carNameInputBtn = document.getElementById("car-names-submit");
-const racingCountBtn = document.getElementById("racing-count-submit");
-carNameInputBtn.addEventListener("click", (e) => e.preventDefault());
-racingCountBtn.addEventListener("click", (e) => e.preventDefault());
+const carNameForm = document.getElementById("car-names-form");
+const inputCountForm = document.getElementById("racing-count-form");
+
+const gameResult = document.getElementById("result");
+
+
+const handleCarNameForm = (e) => {
+  e.preventDefault();
+  if (!inputName.value) {
+    alert("자동차 이름을 입력해주세요");
+    inputName.value = "";
+    return;
+  }
+  if (!inputCount.value) {
+    alert("횟수를 입력해주세요");
+    inputCount.value = "";
+    return;
+  }
+  console.log(inputName.value);
+  if (inputName.value.includes(",")) {
+    const cars = inputName.value.split(",");
+    const carList = [];
+
+    cars.forEach((carName) => {
+      carName && carList.push(new Car(carName));
+    });
+    console.log(carList);
+    for (let i = 0; i < inputCount.value; i++) {
+      carList.forEach((car) => {
+        car.setScore();
+        console.log("name, score: ", car.name, car.score);
+        gameResult.appendChild(car.line);
+      });
+    }
+  } else {
+    new Car(inputName.value);
+  }
+  inputName.value = "";
+};
+
+carNameForm.addEventListener("submit", handleCarNameForm);
+inputCountForm.addEventListener("submit", handleCarNameForm);
