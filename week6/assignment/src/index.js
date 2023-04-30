da92c04a99242f19cdb68bd635e366e39225e622
rbgksqkr <rbgks1937@naver.com>
2023-05-01 00:36:13 +0900

    Feat: 글자수 제한 예외처리

--- week6/assignment/src/index.js
+++ week6/assignment/src/index.js
@@ -9,6 +9,8 @@ const inputCountForm = document.getElementById("racing-count-form");
 
 const carList = new CarList();
 
+let errorFlag = 0;
+
 const handleInputForm = (e) => {
   e.preventDefault();
   if (!inputName.value) {
@@ -16,6 +18,7 @@ const handleInputForm = (e) => {
     inputName.value = "";
     return;
   }
+
   if (!inputCount.value) {
     alert("횟수를 입력해주세요");
     inputCount.value = "";
@@ -26,14 +29,22 @@ const handleInputForm = (e) => {
   if (inputName.value.includes(",")) {
     const cars = inputName.value.split(",");
     cars.forEach((carName) => {
-      carName && carList.addCar(new Car(carName));
+      if (carName.length > 5) errorFlag = 1;
+      !errorFlag && carList.addCar(new Car(carName));
     });
-    carList.showCarList(gameCount);
+    !errorFlag && carList.showCarList(gameCount);
   } else {
-    carList.addCar(new Car(inputName.value));
-    carList.showCarList(gameCount);
+    if (inputName.value.length > 5) errorFlag = 1;
+
+    !errorFlag && carList.addCar(new Car(inputName.value));
+    !errorFlag && carList.showCarList(gameCount);
   }
 
+  if (errorFlag) {
+    alert("자동차 이름을 5자 이내로 입력해주세요");
+    inputName.value = "";
+    inputCount.value = "";
+  }
   carList.showWinner();
   inputName.value = "";
   inputCount.value = "";
