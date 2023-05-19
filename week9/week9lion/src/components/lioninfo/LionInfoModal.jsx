6921399b05cdd7b7f65951c8924e124efeaf59b4
rbgksqkr <rbgks1937@naver.com>
2023-05-19 15:27:43 +0900

    Feat: 페이지넘버 클릭 시 페이지별 데이터 가져오기

--- week9/week9lion/src/components/lioninfo/LionInfoModal.jsx
+++ week9/week9lion/src/components/lioninfo/LionInfoModal.jsx
@@ -52,7 +52,7 @@ const LionInfoModal = () => {
         ))}
       </ButtonDom>
       <UserDataSection userData={userData} />
-      {/* <Pagination /> */}
+      <Pagination setUserData={setUserData} />
     </Dom>
   );
 };
--- week9/week9lion/src/components/lioninfo/Pagination.jsx
+++ week9/week9lion/src/components/lioninfo/Pagination.jsx
@@ -1,24 +1,39 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { getUserPerPage } from "../../apis/lioninfo";
 
-const Pagination = () => {
+const Pagination = (props) => {
+  const { setUserData } = props;
   const [pageNumbers, setPageNumbers] = useState([
-    { id: 1, clicked: false },
-    { id: 2, clicked: false },
-    { id: 3, clicked: false },
-    { id: 4, clicked: false },
-    { id: 5, clicked: false },
-    { id: 6, clicked: false },
-    { id: 7, clicked: false },
+    { id: "1", clicked: true },
+    { id: "2", clicked: false },
+    { id: "3", clicked: false },
+    { id: "4", clicked: false },
+    { id: "5", clicked: false },
+    { id: "6", clicked: false },
+    { id: "7", clicked: false },
   ]);
+
+  const handleClickPage = async (e) => {
+    const response = await getUserPerPage(e.target.id);
+    setPageNumbers(
+      pageNumbers.map((number) =>
+        number.id === e.target.id
+          ? { ...number, clicked: true }
+          : { ...number, clicked: false }
+      )
+    );
+    setUserData(response.data.data);
+  };
 
   return (
     <PaginationDom>
       {pageNumbers.map((number) => (
         <PageNum
-          key={number}
+          key={number.id}
+          id={number.id}
           clicked={number.clicked}
-          onClick={(e) => (e.target.innerText === number.id ? true : false)}
+          onClick={handleClickPage}
         >
           {number.id}
         </PageNum>
