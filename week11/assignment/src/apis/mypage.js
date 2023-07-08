7255678d1ff28fce575d14a50a7cf008193d8eaf
rbgksqkr <rbgks1937@naver.com>
2023-07-08 22:20:24 +0900

    [FEAT] refreshToken 만료 시 로그아웃 시키고 로그인화면으로 이동

--- week11/assignment/src/apis/mypage.js
+++ week11/assignment/src/apis/mypage.js
@@ -4,9 +4,8 @@ import { getRefresh } from "./refresh";
 const baseUrl = "http://front.cau-likelion.org";
 
 export const getMypage = async () => {
+    const token = localStorage.getItem("access");
     try {
-        const token = localStorage.getItem("access");
-
         return await axios.get(`${baseUrl}/mypage`, {
             headers: {
                 Authorization: token,
@@ -15,9 +14,12 @@ export const getMypage = async () => {
     } catch (error) {
         if (error.response.status === 401) {
             // 리프레쉬 토큰을 이용하여 액세스 토큰 발급받기
+            console.log("access token is expired!!!");
             const result = await getRefresh();
-            error.config.headers.Authorization = result.accessToken;
-            return await axios.get(error.config.url, error.config);
+            if (result !== "no refresh") {
+                error.config.headers.Authorization = result.accessToken;
+                return await axios.get(error.config.url, error.config);
+            }
         }
     }
 };
--- week11/assignment/src/apis/refresh.js
+++ week11/assignment/src/apis/refresh.js
@@ -5,15 +5,22 @@ const baseUrl = "http://front.cau-likelion.org";
 export const getRefresh = async () => {
     const refreshToken = localStorage.getItem("refresh");
     const accessToken = localStorage.getItem("access");
-
-    const result = await axios.post(
-        `${baseUrl}/refresh`,
-        { refreshToken },
-        {
-            headers: {
-                Authorization: accessToken,
-            },
+    try {
+        const result = await axios.post(
+            `${baseUrl}/refresh`,
+            { refreshToken },
+            {
+                headers: {
+                    Authorization: accessToken,
+                },
+            }
+        );
+        return result.data;
+    } catch (error) {
+        if (error.response.status === 401) {
+            alert("로그인이 필요합니다.");
+            window.location.href = "http://localhost:3000";
+            return "no refresh";
         }
-    );
-    return result.data;
+    }
 };
