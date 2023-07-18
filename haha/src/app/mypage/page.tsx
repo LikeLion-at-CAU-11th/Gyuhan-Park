db72fb50f8866903e1e53445e36b177f43df530d
rbgksqkr <rbgks1937@naver.com>
2023-07-18 20:41:40 +0900

    [FEAT] 마이페이지 퍼블리싱

---
+++ haha/src/app/mypage/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Title, Wrapper } from "@/components/Common";
+import { useEffect, useState } from "react";
+
+const MyPage = () => {
+    const [data, setData] = useState({
+        name: "",
+        age: 0,
+    });
+
+    return (
+        <Wrapper>
+            <Title>마이페이지</Title>
+            <div>로그인 완료 !</div>
+            <div>
+                <div>{data.name}</div>
+                <div>{data.age}</div>
+            </div>
+        </Wrapper>
+    );
+};
+
+export default MyPage;
--- haha/src/app/page.tsx
+++ haha/src/app/page.tsx
@@ -12,14 +12,16 @@ import Link from "next/link";
 import { useState } from "react";
 import axios from "axios";
 import { login } from "@/apis/login";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
     const [id, setId] = useState("");
     const [pw, setPw] = useState("");
+    const router = useRouter();
 
     const handleSubmitLogin = async (e: any) => {
         e.preventDefault();
-        login(id, pw).then((res) => console.log("!"));
+        login(id, pw).then((res) => router.push("/mypage"));
         setId("");
         setPw("");
     };
