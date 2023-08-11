a31bf6be5d7005902ab63609edfa87325e564b32
rbgksqkr <rbgks1937@naver.com>
2023-08-11 17:29:22 +0900

    [FEAT] styled-component 새로고침 시에도 적용

--- haha/src/app/layout.tsx
+++ haha/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Header from "@/components/common/Header";
+import StyledComponentsRegistry from "./registry";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   );
 }
